78ff0fddb8df0637e68fa2c3867ba7eb23d82d8f
will-3232 <133793296+will-3232@users.noreply.github.com>
2025-04-02 23:05:10 +0300

    Update app.py

--- rsa_calculator/app.py
+++ rsa_calculator/app.py
@@ -72,6 +72,6 @@ def index():
 
     return render_template("index.html", **keys, **messages)
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=3000)
+
