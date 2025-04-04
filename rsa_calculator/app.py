f9fca2d82a00eb883cc496fa5260054794bcc43f
will-3232 <133793296+will-3232@users.noreply.github.com>
2025-04-04 20:41:33 +0300

    Update app.py

--- rsa_calculator/app.py
+++ rsa_calculator/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request
 import random
-import os
 
 app = Flask(__name__)
 
@@ -73,7 +72,7 @@ def index():
 
     return render_template("index.html", **keys, **messages)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=3000)
 
